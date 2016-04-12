9c76f30d851e386e9ab4094e69e68e8bc5d834c2
Svilcata <lord_solen@abv.bg>
2016-04-12 15:56:14 +0200

    completed Challenge Efficient RecyclerView Reloading

--- app/src/main/java/com/svilcata/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/svilcata/criminalintent/CrimeListFragment.java
@@ -25,6 +25,7 @@ public class CrimeListFragment extends Fragment {
 
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
+    private int itemPosition;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -48,7 +49,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
-            mAdapter.notifyDataSetChanged();
+            mAdapter.notifyItemChanged(itemPosition);
         }
     }
 
@@ -76,6 +77,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
+            itemPosition = mCrimeRecyclerView.getChildAdapterPosition(v);
             Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
         }
