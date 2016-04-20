d3e6f39ce806cf1ff58d12c3d9b89203a3f65e45
Svilcata <lord_solen@abv.bg>
2016-04-20 10:38:06 +0300

    fixed github contributions

--- app/src/main/java/com/svilcata/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/svilcata/criminalintent/DatePickerFragment.java
@@ -11,7 +11,6 @@ import android.view.View;
 /**
  * Created by Svilcata on 18-Apr-16.
  */
-//
 public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
