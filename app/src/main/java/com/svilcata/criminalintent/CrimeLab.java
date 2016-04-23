4098e1f41004e9de50580cecdcf3d59920380314
Svilcata <lord_solen@abv.bg>
2016-04-24 01:04:52 +0300

    Chapter 14 - haflway done - added sql database

--- app/src/main/java/com/svilcata/criminalintent/CrimeLab.java
+++ app/src/main/java/com/svilcata/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.svilcata.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.svilcata.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,8 @@ import java.util.UUID;
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -21,6 +26,8 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
         mCrimes = new ArrayList<>();
 
     }
